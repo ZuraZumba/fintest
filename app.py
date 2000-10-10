d5f1d7c1686fcad4c8507d4cf1b7a0b59e173feb
Roman <roman@adontz.com>
2000-10-10 00:00:00 +0000

    Figure memory body them control trouble.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# RIGHT 4 xuabrgqjwtomsephkicdflnzvy
+# RIGHT 3 slnyheagxpudivwqcrzbftokjm
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -20,15 +20,19 @@ class DayOfWeekCalculator:
 
     def print_output(self):
         dd = self.d
-        mm = self.m + 1 if self.m > 2 else self.m + 13
-        yy = self.y if self.m > 2 else self.y - 1
-        b = yy % 100
-        r = yy // 100
-        g = (dd + ((13 * mm) // 5) + b + (b // 4) + (r // 4) - 2 * r + 5) % 7
-        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][g])
+        if self.m >= 3:
+            mm = self.m
+            yy = self.y
+        else:
+            mm = self.m + 12
+            yy = self.y - 1
+        y = yy % 100
+        h = yy // 100
+        e = (dd + ((13 * (mm + 1)) // 5) + y + (y // 4) + (h // 4) - 2 * h + 5) % 7
+        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][e])
 from sys import argv
 
-q = DayOfWeekCalculator(argv[1])
-q.parse_input()
-q.print_output()
+a = DayOfWeekCalculator(argv[1])
+a.parse_input()
+a.print_output()
         
