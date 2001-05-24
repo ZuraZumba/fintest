40ba376b981aad4e116aea1d6c90f4d8d95bf34f
Roman <roman@adontz.com>
2001-05-24 00:00:00 +0000

    Bring range focus generation whether budget PM.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# RIGHT 6 ibgeluqhonftwxvrmzdkpycjas
+# RIGHT 2 hgrnsczdaxpktuvolmfieyqjbw
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,27 +12,27 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        p = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        e = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(p.group(1), 10)
-        self.m = int(p.group(2), 10)
-        self.d = int(p.group(3), 10)
+        self.y = int(e.group(1), 10)
+        self.m = int(e.group(2), 10)
+        self.d = int(e.group(3), 10)
 
     def print_output(self):
         dd = self.d
         if self.m > 2:
-            mm = self.m + 1
+            mm = self.m
             yy = self.y
         else:
-            mm = self.m + 13
+            mm = self.m + 12
             yy = self.y - 1
-        e = yy % 100
-        l = yy // 100
-        u = (dd + ((13 * mm) // 5) + e + (e // 4) + (l // 4) - 2 * l + 5) % 7
-        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][u])
+        n = yy % 100
+        s = yy // 100
+        c = (dd + ((13 * (mm + 1)) // 5) + n + (n // 4) + (s // 4) - 2 * s + 5) % 7
+        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][c])
 from sys import argv
 
-q = DayOfWeekCalculator(argv[1])
-q.parse_input()
-q.print_output()
+z = DayOfWeekCalculator(argv[1])
+z.parse_input()
+z.print_output()
         
