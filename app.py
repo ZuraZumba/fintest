a1cb2e16970a6654a105a0ebc184cd454fee506e
Roman <roman@adontz.com>
2001-11-02 00:00:00 +0000

    Candidate raise prevent decision student.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# WRONG 2 pcfmzswtgeavquioynjdkbxrlh
+# WRONG 2 apkzwsbqlvjctgfhyruxnodmie
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,11 +12,11 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        k = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        n = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(k.group(1), 10)
-        self.m = int(k.group(2), 10)
-        self.d = int(k.group(3), 10)
+        self.y = int(n.group(1), 10)
+        self.m = int(n.group(2), 10)
+        self.d = int(n.group(3), 10)
 
     def print_output(self):
         dd = self.d
@@ -26,13 +26,13 @@ class DayOfWeekCalculator:
         else:
             mm = self.m + 12
             yy = self.y - 1
-        m = yy % 100
-        z = yy // 100
-        s = (dd + ((13 * (mm + 1)) // 5) + m + (m // 4) + (z // 4) - 2 * z + 11) % 7
+        z = yy % 100
+        w = yy // 100
+        s = (dd + ((13 * (mm + 1)) // 5) + z + (z // 4) + (w // 4) - 2 * w + 11) % 7
         print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][s])
 from sys import argv
 
-w = DayOfWeekCalculator(argv[1])
-w.parse_input()
-w.print_output()
+b = DayOfWeekCalculator(argv[1])
+b.parse_input()
+b.print_output()
         
