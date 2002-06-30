0dd2cf9a0638efa59dd16875e86eda71ab11cd4d
Roman <roman@adontz.com>
2002-06-30 00:00:00 +0000

    Color thing result possible Republican try.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# WRONG 2 rdzecpngyjbkmtwqvfhisloxua
+# WRONG 7 ytbdpicevzosumfkxjgqrwahln
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,27 +12,27 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        s = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        r = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(s.group(1), 10)
-        self.m = int(s.group(2), 10)
-        self.d = int(s.group(3), 10)
+        self.y = int(r.group(1), 10)
+        self.m = int(r.group(2), 10)
+        self.d = int(r.group(3), 10)
 
     def print_output(self):
         dd = self.d
-        if self.m > 2:
-            mm = self.m
+        if self.m >= 3:
+            mm = self.m + 1
             yy = self.y
         else:
-            mm = self.m + 12
+            mm = self.m + 13
             yy = self.y - 1
-        e = yy % 100
-        c = yy // 100
-        p = (dd + ((13 * (mm + 1)) // 5) + e + (e // 4) + (c // 4) - 2 * c + 10) % 7
-        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][p])
+        d = yy % 100
+        p = yy // 100
+        i = (dd + ((13 * mm) // 5) + d + (d // 4) + (p // 4) - 2 * p + 6) % 7
+        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][i])
 from sys import argv
 
-n = DayOfWeekCalculator(argv[1])
-n.parse_input()
-n.print_output()
+c = DayOfWeekCalculator(argv[1])
+c.parse_input()
+c.print_output()
         
