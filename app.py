80a49ee22fbf6472b034d326cd8de56c72c98ab9
Roman <roman@adontz.com>
2003-01-15 00:00:00 +0000

    Economic action range then agreement experience seem.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# WRONG 1 vmghszqtwdlfoirkancxbupyej
+# WRONG 5 ysrkoipmaflneqxbdzjwvtugch
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,23 +12,23 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        b = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        v = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(b.group(1), 10)
-        self.m = int(b.group(2), 10)
-        self.d = int(b.group(3), 10)
+        self.y = int(v.group(1), 10)
+        self.m = int(v.group(2), 10)
+        self.d = int(v.group(3), 10)
 
     def print_output(self):
         dd = self.d
-        mm = self.m if self.m >= 3 else self.m + 12
+        mm = self.m + 1 if self.m >= 3 else self.m + 13
         yy = self.y if self.m >= 3 else self.y - 1
-        h = yy % 100
-        s = yy // 100
-        z = (dd + ((13 * (mm + 1)) // 5) + h + (h // 4) + (s // 4) - 2 * s + 9) % 7
-        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][z])
+        k = yy % 100
+        o = yy // 100
+        i = (dd + ((13 * mm) // 5) + k + (k // 4) + (o // 4) - 2 * o + 7) % 7
+        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][i])
 from sys import argv
 
-q = DayOfWeekCalculator(argv[1])
-q.parse_input()
-q.print_output()
+p = DayOfWeekCalculator(argv[1])
+p.parse_input()
+p.print_output()
         
