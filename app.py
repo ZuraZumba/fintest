e9825604900dee85c2a0085fb0df49990c8cce4d
Roman <roman@adontz.com>
2003-08-09 00:00:00 +0000

    Describe leader operation several industry however.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# WRONG 6 evthajurdwikmfbxczpqsoygln
+# WRONG 3 qabixlyjzvmswofgpntchkdreu
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,27 +12,27 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        s = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        h = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(s.group(1), 10)
-        self.m = int(s.group(2), 10)
-        self.d = int(s.group(3), 10)
+        self.y = int(h.group(1), 10)
+        self.m = int(h.group(2), 10)
+        self.d = int(h.group(3), 10)
 
     def print_output(self):
         dd = self.d
-        if self.m > 2:
-            mm = self.m + 1
+        if self.m >= 3:
+            mm = self.m
             yy = self.y
         else:
-            mm = self.m + 13
+            mm = self.m + 12
             yy = self.y - 1
-        h = yy % 100
-        a = yy // 100
-        j = (dd + ((13 * mm) // 5) + h + (h // 4) + (a // 4) - 2 * a + 6) % 7
-        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][j])
+        i = yy % 100
+        x = yy // 100
+        l = (dd + ((13 * (mm + 1)) // 5) + i + (i // 4) + (x // 4) - 2 * x + 10) % 7
+        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][l])
 from sys import argv
 
-u = DayOfWeekCalculator(argv[1])
-u.parse_input()
-u.print_output()
+y = DayOfWeekCalculator(argv[1])
+y.parse_input()
+y.print_output()
         
