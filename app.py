4facdd7a1620988f2368c72a96f7038a4c856b69
Roman <roman@adontz.com>
2003-11-25 00:00:00 +0000

    Hotel wonder would million protect those pattern.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# WRONG 7 qsaejkbyhxmtgvnopufrzlcdwi
+# WRONG 6 xpuzkcgetvlyomhfdaiqbnrjsw
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,27 +12,27 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        z = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        b = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(z.group(1), 10)
-        self.m = int(z.group(2), 10)
-        self.d = int(z.group(3), 10)
+        self.y = int(b.group(1), 10)
+        self.m = int(b.group(2), 10)
+        self.d = int(b.group(3), 10)
 
     def print_output(self):
         dd = self.d
-        if self.m >= 3:
+        if self.m > 2:
             mm = self.m + 1
             yy = self.y
         else:
             mm = self.m + 13
             yy = self.y - 1
-        e = yy % 100
-        j = yy // 100
-        k = (dd + ((13 * mm) // 5) + e + (e // 4) + (j // 4) - 2 * j + 7) % 7
-        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][k])
+        z = yy % 100
+        k = yy // 100
+        c = (dd + ((13 * mm) // 5) + z + (z // 4) + (k // 4) - 2 * k + 7) % 7
+        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][c])
 from sys import argv
 
-b = DayOfWeekCalculator(argv[1])
-b.parse_input()
-b.print_output()
+g = DayOfWeekCalculator(argv[1])
+g.parse_input()
+g.print_output()
         
