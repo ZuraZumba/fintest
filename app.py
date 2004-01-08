988771ee14f7bbae876834563b8bee4bccc84c5d
Roman <roman@adontz.com>
2004-01-08 00:00:00 +0000

    Trade red radio have candidate only cut especially.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# WRONG 5 wtbfpuogkxnvzimyesrhacdljq
+# WRONG 3 jegfvuzboisdlknacmwpqyhtxr
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,23 +12,27 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        a = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        q = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(a.group(1), 10)
-        self.m = int(a.group(2), 10)
-        self.d = int(a.group(3), 10)
+        self.y = int(q.group(1), 10)
+        self.m = int(q.group(2), 10)
+        self.d = int(q.group(3), 10)
 
     def print_output(self):
         dd = self.d
-        mm = self.m + 1 if self.m >= 3 else self.m + 13
-        yy = self.y if self.m >= 3 else self.y - 1
+        if self.m >= 3:
+            mm = self.m
+            yy = self.y
+        else:
+            mm = self.m + 12
+            yy = self.y - 1
         f = yy % 100
-        p = yy // 100
-        u = (dd + ((13 * mm) // 5) + f + (f // 4) + (p // 4) - 2 * p + 9) % 7
+        v = yy // 100
+        u = (dd + ((13 * (mm + 1)) // 5) + f + (f // 4) + (v // 4) - 2 * v + 11) % 7
         print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][u])
 from sys import argv
 
-o = DayOfWeekCalculator(argv[1])
-o.parse_input()
-o.print_output()
+z = DayOfWeekCalculator(argv[1])
+z.parse_input()
+z.print_output()
         
