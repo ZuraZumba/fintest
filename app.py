5f2cdd6c9fe964c5cbbc79d6092f64dfffef1961
Roman <roman@adontz.com>
2004-02-02 00:00:00 +0000

    Word join debate if west exactly.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# WRONG 2 iupcwvdfyhjmqgslzarekbotnx
+# WRONG 7 fxomlsrcvtbpjehwgadiqnkuyz
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,27 +12,27 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        k = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        q = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(k.group(1), 10)
-        self.m = int(k.group(2), 10)
-        self.d = int(k.group(3), 10)
+        self.y = int(q.group(1), 10)
+        self.m = int(q.group(2), 10)
+        self.d = int(q.group(3), 10)
 
     def print_output(self):
         dd = self.d
-        if self.m > 2:
-            mm = self.m
+        if self.m >= 3:
+            mm = self.m + 1
             yy = self.y
         else:
-            mm = self.m + 12
+            mm = self.m + 13
             yy = self.y - 1
-        c = yy % 100
-        w = yy // 100
-        v = (dd + ((13 * (mm + 1)) // 5) + c + (c // 4) + (w // 4) - 2 * w + 9) % 7
-        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][v])
+        m = yy % 100
+        l = yy // 100
+        s = (dd + ((13 * mm) // 5) + m + (m // 4) + (l // 4) - 2 * l + 11) % 7
+        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][s])
 from sys import argv
 
-d = DayOfWeekCalculator(argv[1])
-d.parse_input()
-d.print_output()
+r = DayOfWeekCalculator(argv[1])
+r.parse_input()
+r.print_output()
         
