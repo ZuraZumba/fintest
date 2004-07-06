e02356b3eef32c3a770f0540329b4f57db1ae8d6
Roman <roman@adontz.com>
2004-07-06 00:00:00 +0000

    Pull common late player whether place people.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# WRONG 6 dwvtibmezrhgxjaqyoknfsupcl
+# WRONG 3 gdynabiusmelwcxqfrtzvhpkoj
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,27 +12,27 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        f = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        v = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(f.group(1), 10)
-        self.m = int(f.group(2), 10)
-        self.d = int(f.group(3), 10)
+        self.y = int(v.group(1), 10)
+        self.m = int(v.group(2), 10)
+        self.d = int(v.group(3), 10)
 
     def print_output(self):
         dd = self.d
-        if self.m > 2:
-            mm = self.m + 1
+        if self.m >= 3:
+            mm = self.m
             yy = self.y
         else:
-            mm = self.m + 13
+            mm = self.m + 12
             yy = self.y - 1
-        t = yy % 100
-        i = yy // 100
-        b = (dd + ((13 * mm) // 5) + t + (t // 4) + (i // 4) - 2 * i + 7) % 7
+        n = yy % 100
+        a = yy // 100
+        b = (dd + ((13 * (mm + 1)) // 5) + n + (n // 4) + (a // 4) - 2 * a + 6) % 7
         print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][b])
 from sys import argv
 
-m = DayOfWeekCalculator(argv[1])
-m.parse_input()
-m.print_output()
+i = DayOfWeekCalculator(argv[1])
+i.parse_input()
+i.print_output()
         
