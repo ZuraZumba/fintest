8a6aee5a32254bc85529da7880aefd06d381571c
Roman <roman@adontz.com>
2004-08-08 00:00:00 +0000

    Law hundred mention over outside can.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# WRONG 0 vejrxqfguonwsblzmdaichktpy
+# WRONG 5 etifjwxbsordakpngyuhqlmcvz
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,23 +12,23 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        c = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        q = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(c.group(1), 10)
-        self.m = int(c.group(2), 10)
-        self.d = int(c.group(3), 10)
+        self.y = int(q.group(1), 10)
+        self.m = int(q.group(2), 10)
+        self.d = int(q.group(3), 10)
 
     def print_output(self):
         dd = self.d
-        mm = self.m if self.m > 2 else self.m + 12
-        yy = self.y if self.m > 2 else self.y - 1
-        r = yy % 100
-        x = yy // 100
-        q = (dd + ((13 * (mm + 1)) // 5) + r + (r // 4) + (x // 4) - 2 * x + 11) % 7
-        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][q])
+        mm = self.m + 1 if self.m >= 3 else self.m + 13
+        yy = self.y if self.m >= 3 else self.y - 1
+        f = yy % 100
+        j = yy // 100
+        w = (dd + ((13 * mm) // 5) + f + (f // 4) + (j // 4) - 2 * j + 11) % 7
+        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][w])
 from sys import argv
 
-f = DayOfWeekCalculator(argv[1])
-f.parse_input()
-f.print_output()
+x = DayOfWeekCalculator(argv[1])
+x.parse_input()
+x.print_output()
         
