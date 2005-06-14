e69ad83d71337bbe1f317b6569b7c062952769fa
Roman <roman@adontz.com>
2005-06-14 00:00:00 +0000

    Ability sound history everyone present.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# WRONG 7 ytbdpicevzosumfkxjgqrwahln
+# WRONG 5 fasxygmvjrtblzqpneudokcwhi
 
 class DayOfWeekCalculator:
     def __init__(self, input:str):
@@ -12,27 +12,23 @@ class DayOfWeekCalculator:
     def parse_input(self):
         from re import fullmatch
 
-        r = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
+        o = fullmatch("(\d{4})-(\d{2})-(\d{2})", self.i)
 
-        self.y = int(r.group(1), 10)
-        self.m = int(r.group(2), 10)
-        self.d = int(r.group(3), 10)
+        self.y = int(o.group(1), 10)
+        self.m = int(o.group(2), 10)
+        self.d = int(o.group(3), 10)
 
     def print_output(self):
         dd = self.d
-        if self.m >= 3:
-            mm = self.m + 1
-            yy = self.y
-        else:
-            mm = self.m + 13
-            yy = self.y - 1
-        d = yy % 100
-        p = yy // 100
-        i = (dd + ((13 * mm) // 5) + d + (d // 4) + (p // 4) - 2 * p + 6) % 7
-        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][i])
+        mm = self.m + 1 if self.m >= 3 else self.m + 13
+        yy = self.y if self.m >= 3 else self.y - 1
+        x = yy % 100
+        y = yy // 100
+        g = (dd + ((13 * mm) // 5) + x + (x // 4) + (y // 4) - 2 * y + 7) % 7
+        print(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][g])
 from sys import argv
 
-c = DayOfWeekCalculator(argv[1])
-c.parse_input()
-c.print_output()
+m = DayOfWeekCalculator(argv[1])
+m.parse_input()
+m.print_output()
         
